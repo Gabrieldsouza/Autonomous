9138500d6adc0f83b3e5ab1e99ee352900f07abe
Patrick Amaral <patrick.dev.atom@gmail.com>
2019-03-25 22:57:25 -0300

    Incluido Service para pegar do nosso webservice, e deixei um exemplo da instancia funcionando.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AutonomosService} from './autonomos/autonomos.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      HttpClientModule,
+      IonicModule.forRoot(),
+      AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AutonomosService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
